fcebfa7905073e7c7984f660de3ee5a549a5b9b8
tayler <pochtaAPT@mail.ru>
2025-09-22 09:19:22 +0800

    изучил все виды кунг фу

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-a
+console.log('САЛАМ АЛЕЙКУМ');
+console.log("ВАЛЕЙКУМ ВАСАЛАМ");
+
+
+console.log(13);
+console.log('13');
+
+let _str1 = 'всем хаю хай я лох('
+let _str2 = 'эх всем привет я сима'
+let _str3 = 'ХЕЛЛОУ! я грустный чувак( #депрессия'
+let _str4 = 'ЗАКРОЙ СВОЙ РОТТТТ Я ПСИХХХХ!'
+
+console.log(_str1 + _str3 +  _str2 + _str4)
+
+let name = 'TOXICH'
+let age = '24'
+
+console.log(age - 5 + 'age' + name)
