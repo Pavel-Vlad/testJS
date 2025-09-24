b331967c568ce60ffc370714aee0d7709d236161
sewiks4 <adad@gmail.com>
2025-09-24 09:17:49 +0800

    калькулятор

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-let grill= document.querySelector('input')
 
-console.log(grill.value)
-grill.addEventListener('input',function (e){
-console.log(e.target.value)
+let sumButton= document.getElementById('sum')
+let minButton= document.getElementById('minus')
+let delButton= document.getElementById('del')
+let ymnButton= document.getElementById('ymn')
+let answerButton= document.getElementById('answer')
+sumButton.addEventListener('click',function (e){
+    let number1= +document.getElementById('num1').value
+    let number2= +document.getElementById('num2').value
+    answerButton.value = number1 + number2
+})
+minButton.addEventListener('click',function (e){
+    let number1= +document.getElementById('num1').value
+    let number2= +document.getElementById('num2').value
+    answerButton.value = number1 - number2
+})
+delButton.addEventListener('click',function (e){
+    let number1= +document.getElementById('num1').value
+    let number2= +document.getElementById('num2').value
+    answerButton.value = number1 / number2
+})
+ymnButton.addEventListener('click',function (e){
+    let number1= +document.getElementById('num1').value
+    let number2= +document.getElementById('num2').value
+    answerButton.value = number1 * number2
 })
