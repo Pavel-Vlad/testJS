2b806489b47333a077ed5b0f2a88948e3acc806c
tayler <pochtaAPT@mail.ru>
2025-09-24 09:21:16 +0800

    изучил все виды кунг фу x2

--- js/1.js
+++ js/1.js
@@ -1,10 +1,33 @@
-let kryakozyabra = document.querySelector('input');
+let sumButton = document.getElementById('sum')
+let minusButton = document.getElementById('minus')
+let zvezdaButton = document.getElementById('zvezda')
+let slashButton = document.getElementById('slash')
+let answer = document.getElementById('answer')
 
-console.log(kryakozyabra)
 
-kryakozyabra.addEventListener('input', function (e) {
-    console.log(e.target.value)
+sumButton.addEventListener('click', function (e){
+    let number1 = +document.getElementById('num1').value;
+    let number2 = +document.getElementById('num2').value;
+    answer.value = number1 + number2;
 })
 
 
+minusButton.addEventListener('click', function (e){
+    let number1 = +document.getElementById('num1').value;
+    let number2 = +document.getElementById('num2').value;
+    answer.value = number1 - number2;
+})
 
+
+slashButton.addEventListener('click', function (e){
+    let number1 = +document.getElementById('num1').value;
+    let number2 = +document.getElementById('num2').value;
+    answer.value = number1 / number2;
+})
+
+
+zvezdaButton.addEventListener('click', function (e){
+    let number1 = +document.getElementById('num1').value;
+    let number2 = +document.getElementById('num2').value;
+    answer.value = number1 * number2;
+})
