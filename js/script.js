1cd10cf62d3919016f2e68f778718bab625a8572
Pavel Vladimirovich <sawel@yandex.ru>
2025-09-24 09:21:02 +0800

    создали калькулятор

--- js/script.js
+++ js/script.js
@@ -1,7 +1,41 @@
-let dragosyabra = document.querySelector('input');
-let str = 'weewf';
+let sumButton = document.getElementById('sum');
+let minButton = document.getElementById('min');
+let umnButton = document.getElementById('umn');
+let razButton = document.getElementById('raz');
+let answer = document.getElementById('answer');
+sumButton.addEventListener(
+    'click',
+    function (e) {
+        let number1 = +document.getElementById('num1').value;
+        let number2 = +document.getElementById('num2').value;
+        answer.value = number1 + number2;
+    }
+)
 
-dragosyabra.addEventListener('input', function (e) {
-    console.log(e.target.value)
-})
+minButton.addEventListener(
+    'click',
+    function (e) {
+        let number1 = +document.getElementById('num1').value;
+        let number2 = +document.getElementById('num2').value;
+        answer.value = number1 - number2;
+    }
+)
+
+umnButton.addEventListener(
+    'click',
+    function (e) {
+        let number1 = +document.getElementById('num1').value;
+        let number2 = +document.getElementById('num2').value;
+        answer.value = number1 * number2;
+    }
+)
+
+razButton.addEventListener(
+    'click',
+    function (e) {
+        let number1 = +document.getElementById('num1').value;
+        let number2 = +document.getElementById('num2').value;
+        answer.value = number1 / number2;
+    }
+)
 
